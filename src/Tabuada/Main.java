1d5d291ba7069132a3b1db36bc31e4416faa30ea
Yan <yangarridodev@gmail.com>
2025-05-02 10:57:44 -0300

    Tabuada de um numero custumizado até a quantidade definida pelo usuário

---
+++ src/Tabuada/Main.java
@@ -0,0 +1,26 @@
+package Tabuada;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escreva o um número desejado: ");
+        int num = sc.nextInt();
+        System.out.print("Até onde a tabuada dever ir: ");
+        int quant = sc.nextInt();
+
+        tabuadaDeNumero(num, quant);
+    }
+    public static void tabuadaDeNumero(int num, int quant){
+        for(int i = 0; i <= quant; i++){
+            int resultado = num * i;
+            System.out.println("Resultado de: " + num + " * " + i + " = " + resultado);
+
+        }
+
+
+    }
+}

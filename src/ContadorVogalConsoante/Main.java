514f2dd232b390eebbf0ff6162641779138194d6
Yan <yangarridodev@gmail.com>
2025-05-02 13:38:23 -0300

    refatoração, criação de metodo para contar cada caractere

--- src/ContadorVogalConsoante/Main.java
+++ src/ContadorVogalConsoante/Main.java
@@ -8,34 +8,69 @@ public class Main {
         System.out.println("Digite uma frase abaixo:");
         String frase = sc.nextLine().toLowerCase();
 
+
+        System.out.println("Quantidade de vogais: " + contaVogal(frase));
+        System.out.println("Quantidade de consoantes: " + contaConsoante(frase));
+        System.out.println("Quantidade de espaços: " + contaEspacos(frase));
+        System.out.println("Quantidade de números: " + contaNum(frase));
+        System.out.println("Quantidade de pontuações: " + contaPontuacao(frase));
+    }
+
+
+    public static int contaVogal(String frase) {
         int quantVogais = 0;
-        int quantConsoantes = 0;
-        int quantEspacos = 0;
-        int quantNum = 0;
-        int quantPontuacao = 0;
-        for (int i = 0; i < frase.length(); i++){
+
+        for (int i = 0; i < frase.length(); i++) {
             char c = frase.charAt(i);
-            if(Character.isLetter(c)){
-                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-                    quantVogais ++ ;
-
-                } else {
-                    quantConsoantes ++;
+            if (Character.isLetter(c)) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    quantVogais++;
                 }
-            } else if(frase.charAt(i) == ' ') {
-                quantEspacos ++;
-
-            } else if (Character.isDigit(c)) {
-                quantNum ++;
-            } else {
-                quantPontuacao ++;
             }
 
         }
-        System.out.println("Quantidade de vogais: " + quantVogais);
-        System.out.println("Quantidade de consoantes: " + quantConsoantes);
-        System.out.println("Quantidade de espaços: " + quantEspacos);
-        System.out.println("Quantidade de números: " + quantNum);
-        System.out.println("Quantidade de pontuações: " + quantPontuacao);
+        return quantVogais;
+    }
+    public static int contaConsoante(String frase){
+        int quantConsoante = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+            if (Character.isLetter(c)) {
+                if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                    quantConsoante++;
+                }
+            }
+        }
+        return quantConsoante;
+    }
+    public static int contaEspacos(String frase){
+        int quantEspacos = 0;
+        for (int i = 0; i < frase.length(); i++){
+            if(frase.charAt(i) == ' '){
+                quantEspacos ++;
+            }
+        }
+        return  quantEspacos;
+    }
+    public static int contaNum(String frase){
+        int quantNum = 0;
+        for (int i = 0; i < frase.length(); i++){
+            char c = frase.charAt(i);
+            if(Character.isDigit(c)){
+                quantNum ++;
+            }
+        }
+        return quantNum;
+    }
+
+    public static int contaPontuacao(String frase){
+        int quantPontuacao = 0;
+        for (int i = 0; i < frase.length(); i ++){
+            char c = frase.charAt(i);
+            if(!Character.isLetterOrDigit(c) && c != ' ' ){
+                quantPontuacao ++;
+            }
+        }
+        return quantPontuacao;
     }
 }

cd893f76e92c75eb35f78388c8d3eff27bc04b51
Yan <yangarridodev@gmail.com>
2025-05-02 12:37:05 -0300

    Tabuada em Java

--- src/Tabuada/Main.java
+++ src/Tabuada/Main.java
@@ -5,17 +5,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Escreva o um número desejado: ");
-        int num = sc.nextInt();
-        System.out.print("Até onde a tabuada dever ir: ");
-        int quant = sc.nextInt();
+        System.out.print("Escreva o número de inicio da tabuada: ");
+        int inicio = sc.nextInt();
+        System.out.print("Escreva o número final da tabuada: ");
+        int fim =sc.nextInt();
+        if(fim < inicio){
+            System.out.println("Fim não pode ser menor que o inicio");
 
-        tabuadaDeNumero(num, quant);
+        } else {
+            System.out.print("Até onde a tabuada dever ir: ");
+            int limite = sc.nextInt();
+
+            for(int num = inicio; num<= fim; num++){
+                System.out.println(" ");
+                System.out.println("TABUADA DO NÚMERO:" + num);
+                System.out.println(" ");
+                tabuadaDeNumero(num, limite);
+            }
+        }
+
+
     }
-    public static void tabuadaDeNumero(int num, int quant){
-        for(int i = 0; i <= quant; i++){
+    public static void tabuadaDeNumero(int num, int limite){
+        for(int i = 0; i <= limite; i++){
             int resultado = num * i;
-            System.out.println("Resultado de: " + num + " * " + i + " = " + resultado);
+            System.out.println(num + " * " + i + " = " + resultado);
 
         }
 

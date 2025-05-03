e2dd256b47c68bb066808c18213c168b91fd948c
Yan <yangarridodev@gmail.com>
2025-05-03 10:32:31 -0300

    ajuste nos metodos

--- src/AgendaDeContato/Agenda.java
+++ src/AgendaDeContato/Agenda.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Agenda {
 
-    ArrayList<ArrayList<String>> contatos = new ArrayList<>();
+    ArrayList<Contato> contatos = new ArrayList<>();
 
     public static void main(String[] args) {
         Agenda contatos = new Agenda();
@@ -24,6 +24,7 @@ public class Agenda {
             System.out.print("Escolha sua opção: ");
             escolha = sc.nextInt();
             sc.nextLine();
+            System.out.println(" ");
 
             switch (escolha){
                 case 1:{
@@ -34,7 +35,7 @@ public class Agenda {
                     System.out.print("Escreva o e-mail: ");
                     email = sc.nextLine();
                     Contato contato = new Contato(nome,telefone,email);
-                    contatos.adicionar(contato.nome, contato.telefone, contato.email);
+                    contatos.adicionar(contato);
                     break;
                 }
                 case 2:{
@@ -68,38 +69,33 @@ public class Agenda {
         }
     }
 
-    public  void adicionar(String nome, String telefone, String email){
-        ArrayList<String> contato = new ArrayList<>();
-        contato.add(nome);
-        contato.add(telefone);
-        contato.add(email);
-        System.out.println(contato);
+    public  void adicionar(Contato contato){
         contatos.add(contato);
-        System.out.println(contatos);
+        System.out.println(" ");
     }
     public void listar(){
         for(int i = 0; i < contatos.size(); i++){
-            System.out.println(contatos.get(i));
+            System.out.println("Nome: " + contatos.get(i).nome + " Telefone: " + contatos.get(i).telefone + " E-mail: " + contatos.get(i).email);
+            System.out.println(" ");
         }
     }
     public void buscar(String nome){
         for(int i = 0; i < contatos.size(); i++){
-            for(int j = 0; j < contatos.get(i).size(); j++ ){
-                if(contatos.get(i).get(j).equals(nome)){
-                    System.out.println(contatos.get(i));
-                }
+            if(contatos.get(i).nome.equals(nome)){
+                System.out.println("Nome: " + contatos.get(i).nome + " Telefone: " + contatos.get(i).telefone + " E-mail: " + contatos.get(i).email);
+                System.out.println(" ");
             }
         }
     }
     public String remover(String nome){
         for(int i = 0; i < contatos.size(); i++){
-            for(int j = 0; j < contatos.get(i).size(); j++ ){
-                if(contatos.get(i).get(j).equals(nome)){
+            if(contatos.get(i).nome.equals(nome)){
                     contatos.remove(i);
                 }
-            }
+
         }
         return "Contato removido com sucesso";
+
     }
 
 }

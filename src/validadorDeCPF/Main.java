c599fbe343cd0362a08cd25c4c9a7b095810a396
Yan <yangarridodev@gmail.com>
2025-05-05 11:39:18 -0300

    melhor interface, correções de escritas, transformação dos metodos da Classe Validador para static e outras alterações

--- src/validadorDeCPF/Main.java
+++ src/validadorDeCPF/Main.java
@@ -6,11 +6,13 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
   public static void main(String[] args) {
-    Validador validador = new Validador();
     Scanner sc = new Scanner(System.in);
+    System.out.println("VALIDADOR DE CPF");
+    System.out.println(" ");
     System.out.print("Digite um CPF: ");
     String cpf = sc.nextLine();
-    System.out.println(validador.validarCpf(cpf));
+    System.out.println("Resposta: ");
+    System.out.println(Validador.validarCpf(cpf));
 
   }
 
--- src/validadorDeCPF/Validador.java
+++ src/validadorDeCPF/Validador.java
@@ -4,19 +4,19 @@ import static java.lang.Integer.parseInt;
 
 public class Validador {
 
-  public int calcularVerificadorUm(String cpf){
+  public static int calcularVerificadorUm(String cpf){
     int digitoVerificadorUm = 0;
     String noveDigitos = cpf.substring(0,9);
 
-    int num1 = 0;
+    int somaPonderada = 0;
     for(int i = 0; i < noveDigitos.length(); i ++){
       int num = parseInt(String.valueOf(noveDigitos.charAt(i)));
 
-      num1 = num1 + num * (10 - i);
+      somaPonderada = somaPonderada + num * (10 - i);
 
 
     }
-    int resto = num1 % 11;
+    int resto = somaPonderada % 11;
     if(resto < 2){
       digitoVerificadorUm = 0;
     } else if(resto >= 2){
@@ -25,7 +25,7 @@ public class Validador {
 
     return digitoVerificadorUm;
   }
-  public int calcularVerificadorDois(String cpf, int digitoVerificadorUm){
+  public static int calcularVerificadorDois(String cpf, int digitoVerificadorUm){
     int digitoVerificadorDois = 0;
     String digitoVerificador = String.valueOf(digitoVerificadorUm);
     String noveDigitos = cpf.substring(0,9);
@@ -49,16 +49,18 @@ public class Validador {
     return digitoVerificadorDois;
   }
 
-  public String validarCpf(String cpf){
+  public static String validarCpf(String cpf){
     cpf = cpf.replaceAll("\\D","");
-    int verificadorUm = calcularVerificadorUm(cpf);
+
     if(cpf.length() != 11){
-      return "CPF INVÁLiDO";
+      return "CPF INVÁLIDO";
     }
 
     if(cpf.chars().distinct().count() == 1){
       return "CPF INVÁLIDO";
     }
+
+    int verificadorUm = calcularVerificadorUm(cpf);
 
     if(verificadorUm != Character.getNumericValue(cpf.charAt(9))){
       return "CPF INVÁLIDO";
